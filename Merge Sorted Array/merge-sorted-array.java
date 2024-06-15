8d22e56d652c066b91b65398daa1ceb3d03f92be
Amelia Dutta <49182604+amelia2802@users.noreply.github.com>
2024-06-15 21:41:50 +0530

    Update merge-sorted-array.java

--- Merge Sorted Array/merge-sorted-array.java
+++ Merge Sorted Array/merge-sorted-array.java
@@ -1,5 +1,7 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        //Time complexity O(m+n)
         int i = m-1;
         int j = n-1;
         int k = nums1.length-1;
@@ -16,5 +18,17 @@ class Solution {
         while(j>=0) {
         nums1[k--] = nums2[j--];
         }
+
+        //Time complexityO((m + n) log (m + n))
+        int j =0;
+
+        for(int i=m;i<nums1.length;i++){
+            if(j<nums2.length){
+                nums1[i] = nums2[j];
+            }
+            j++;
+        }
+
+        Arrays.sort(nums1);
     }
 }
