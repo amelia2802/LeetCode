f849566761720980a1ea0efad7a613dca31e96d8
Amelia Dutta <49182604+amelia2802@users.noreply.github.com>
2024-11-05 20:17:06 +0530

    Update 0two-sum.java

--- 0two-sum/0two-sum.java
+++ 0two-sum/0two-sum.java
@@ -1,19 +1,22 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
-        HashMap<Integer,Integer> temp_map = new HashMap<>();
-        
-        for(int i=0;i<nums.length;i++){
+
+        //initialize an empty map to store the values
+        HashMap<Integer,Integer> tempMap = new HashMap<>();
+
+        for(int i =0;i<nums.length;i++){
+            //find the second number by target - nums[i] 
             int secondNum = target - nums[i];
-            
-            if(temp_map.containsKey(secondNum)){
-                return new int[]{temp_map.get(secondNum),i};
+
+            //check if the second number presents in the map and return the indexes of both
+            if(tempMap.containsKey(secondNum)){
+                return new int[]{tempMap.get(secondNum),i};
             }
-            
-            temp_map.put(nums[i],i);
-            
+
+            //add the current array element to the map
+            tempMap.put(nums[i],i);
         }
-        
-        return new int[] {};
+
+        return new int[]{};
     }
 }
