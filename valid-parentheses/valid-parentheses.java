4ee24c16ab2f213f846a881adea5ea1e64928234
Amelia Dutta <49182604+amelia2802@users.noreply.github.com>
2024-11-15 20:13:17 +0530

    Time: 3 ms, Memory: 41.3 MB - LeetHub

--- valid-parentheses/valid-parentheses.java
+++ valid-parentheses/valid-parentheses.java
@@ -1,15 +1,12 @@
 class Solution {
     public boolean isValid(String s) {
+        Stack<Character> tempStack = new Stack<>(); //initialize an empty stack
         
-        s.replaceAll("\\s","");
-        
-        Stack<Character> tempStack = new Stack<>();
-        
-        for(int i=0;i<s.length();i++){
+        for(int i=0;i<s.length();i++){  //loop through the string
             
             char tempChar = s.charAt(i);
            
-            if( tempChar == '(' || tempChar == '{' || tempChar == '[' ){
+            if( tempChar == '(' || tempChar == '{' || tempChar == '[' ){ //if the indexed character of the given string is opening bracket then push it into the stack
                 
                 tempStack.push(tempChar);
                 continue;
@@ -17,10 +14,12 @@ class Solution {
             
             else{
                 
+                //base case
                 if (tempStack.isEmpty()) {
                     return false;
                 }
                 
+                //if the indexed character is a closing bracket and the top of the stack was the opening for it then pop
                 if (
                     (tempChar == ')' && tempStack.peek() == '(' ) || 
                     (tempChar == '}' && tempStack.peek() == '{' ) || 
@@ -29,12 +28,13 @@ class Solution {
                 {
                     tempStack.pop();
                 }
-                else{
+                else{   //if not then return false
                     return false;
                 }
             }
         }
         
+        //if the stack is empty that means we popped all the stack elements so it's a valid parentheses
         return tempStack.isEmpty();
     }
 }
